290d38a2a584611d18542c985e0505a3cd01965e
kaif-lucifer <85788390+kaif-lucifer@users.noreply.github.com>
2023-12-24 02:13:30 +0530

    Create hangman_words.py

---
+++ The_HangMan_Game-My_Version/hangman_words.py
@@ -0,0 +1,13 @@
+Fruits= [ "Apple", "Orange", "Banana", "Grapes", "Watermelon", "Mango", "Kiwi", "Pineapple", "Peach", "Plum", "Pear", "Cherry", "Strawberry", "Blueberry", "Raspberry", "Blackberry", "Melon", "Papaya","Musklemon", "Pomegranate"]
+Vegetables = ["Carrot", "Cabbage", "Potato", "Onion", "Tomato", "Raddish", "Pumpkin", "Peas", "Cucumber", "Broccoli", "Lettuce", "Cauliflower", "Brussels Sprouts", "Squash", "Spinach","Turnip" ]
+Animals = ["Dog", "Cat", "Elephant", "Lion", "Tiger","Penguin", "Giraffe", "Zebra", "Eagle", "Bear", "Bat", "Fox", "Rhino", "Gorilla", "Squirrel", "Hippopotamus", "Otter", "Snake", "Turtle", "Frog", "Crocodile", "Alligator", "Crab", "Snail", "Clam", "Lobster", "Shrimp", "Squid"]
+Colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Pink", "Brown", "Black", "White", "Gray", "Silver", "Gold", "Violet", "Lime", "Cyan", "Maroon", "Teal", "Navy", "Olive"]
+Countries = ["India", "China", "Japan", "South Africa", "Nepal", "Brazil", "Australia", "Argentina", "Mexico", "Canada", "Denmark", "Norway", "New Zealand", "Sweden", "Norway", "Switzerland", "Iceland", "Finland", "Iceland"]
+Cities = ["Mumbai", "Delhi", "Kolkata", "Bangalore", "Chennai", "Hyderabad", "Kolkata", "Singapore", "Sydney", "Melbourne", "Seoul", "Shanghai", "Beijing", "Hong Kong", "Shanghai", "Seoul", "Tokyo", "Osaka"]
+Foods = ["Pizza", "Pasta", "Burger", "Noodles", "Rice", "Chicken", "Fish", "Fries", "Salad", "Sandwiches", "Samosa", "Chocolate", "Cake", "Ice Cream", "Cheese", "Chocolate" ]
+Objects = ["Pen", "Pencil", "Eraser", "Ruler", "Calculator", "Book", "Notebook", "Folder", "Backpack", "Shoes", "Socks", "Hat", "Gloves", "Sunglasses" ]
+
+words_list =[Fruits,Vegetables, Animals, Colors,Countries, Cities, Foods ,Objects]
+
+def show():
+  print(words_list)
